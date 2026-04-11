7a8d08a8b7af8fd66c237b28a5c6e7cd3af535c6
Timothy Tan <timothytan94@gmail.com>
2026-04-11 16:55:55 +0800

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        List<Integer> ignoreIndex = new ArrayList<>();
+        List<String> toAddtoResult=new ArrayList<>();
+        for(int i=0;i<strs.length;i++){
+          if(!ignoreIndex.contains(i)){
+          toAddtoResult.add(strs[i]);
+          Map<Character, Integer> countMapI = createHashMapCharacterCount(strs[i]);
+          for(int j=i+1;j<strs.length;j++){
+            if(strs[i].length() == strs[j].length()){
+              Map<Character, Integer> countMapJ = createHashMapCharacterCount(strs[j]);
+              if(countMapI.equals(countMapJ)){
+                toAddtoResult.add(strs[j]);
+                ignoreIndex.add(j);
+              }
+            }
+          }
+
+          result.add(toAddtoResult);
+          toAddtoResult = new ArrayList<>();
+        }
+        }
+        return result;
+    }
+
+    public Map<Character, Integer> createHashMapCharacterCount(String str){
+      Map<Character, Integer> countMap = new HashMap<>();
+          for(Character c : str.toCharArray()){
+            if(countMap.get(c)!=null){
+              int val = countMap.get(c);
+              countMap.put(c, val+1);
+            }else{
+              countMap.put(c, 1);
+            }
+          }
+          return countMap;
+    }
+}
+
+/*
+pick string, move string to array
+make hashmap1 {key=char, value=count}
+check next string, if length is same
+    make hashmap2 {key=char, value=count}
+    compare with hashmap1, if same, add to array
+    move string to array
+*/
